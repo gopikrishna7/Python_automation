63d57ce122ceb3495b6cbbe8afce91ad3fbc02b0
krishna <gopikrishna.mgk7@gmail.com>
2023-03-09 09:45:13 +0530

    add

--- test.py
+++ test.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 
 
-import shutil
-import psutil
+import requests
 
-def check_disk_usage(disk):
-    du=shutil.disk_usage(disk)
-    free=du.free/du.total*100
-    return free>20
+r = requests.get("http://example.com")
 
-def check_cpu_usage():
-    cpu_usage=psutil.cpu_percent(1)
-    return cpu_usage < 75
-
-if not check_disk_usage("/") or not check_cpu_usage():
-    print("ERROR")
+if r.status_code==200:
+    print("Everything Looks Good!")
 else:
-    print("Everything is Ok")
+    print("Something went wrong!")
+
+
+
